0e656c235730f1c29362e4d1b2eb1089007372ee
Melih Dural <duralmelih@gmail.com>
2022-08-14 23:15:31 +0300

    fight screen merge bug fixed

--- Assets/Merge System Free/Scripts/GameController.cs
+++ Assets/Merge System Free/Scripts/GameController.cs
@@ -82,7 +82,6 @@ public class GameController : MonoBehaviour
 
             if (!isMouseUp && slot.state == SlotState.Full && carryingItem == null)
             {
-                Debug.Log(hit.collider.gameObject.transform.GetChild(0).gameObject.tag);
                 if (hit.collider.gameObject.transform.GetChild(0).gameObject.CompareTag("2D"))
                 {
                     var itemGO = (GameObject) Instantiate(Resources.Load("Prefabs/ItemDummy"));
@@ -205,9 +204,19 @@ public class GameController : MonoBehaviour
 
     void OnItemCarryFail()
     {
-        var slot = GetSlotById(carryingItem.slotId);
-        slot.CreateItem(carryingItem.itemId);
-        Destroy(carryingItem.gameObject);
+        if (carryingItem.gameObject.CompareTag("2D"))
+        {
+            var slot = GetSlotById(carryingItem.slotId);
+            slot.CreateItem(carryingItem.itemId);
+            Destroy(carryingItem.gameObject);
+        }
+        else if (carryingItem.gameObject.CompareTag("3D"))
+        {
+            var slot = GetSlotById(carryingItem.slotId);
+            slot.CreateItem3D(carryingItem.itemId);
+            Destroy(carryingItem.gameObject);
+        }
+        
     }
 
     public void PlaceRandomArcher()
