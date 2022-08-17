a55a2d1444f3dac1c6834ce1cd959e92263274c2
Melih Dural <93449688+melihdural@users.noreply.github.com>
2022-08-17 20:31:04 +0300

    Asset Folder edited

---
+++ Assets/Scripts/Grid/Slot.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Slot : MonoBehaviour
+{
+    public int id;
+    public Item currentItem;
+    public SlotState state = SlotState.Empty;
+
+    public void CreateItem(int id) 
+    {
+        var itemGO = (GameObject)Instantiate(Resources.Load("Prefabs/Grid/Item"));
+        
+        itemGO.transform.SetParent(this.transform);
+        itemGO.transform.localPosition = Vector3.zero;
+        itemGO.transform.localScale = Vector3.one;
+
+        currentItem = itemGO.GetComponent<Item>();
+        currentItem.Init(id, this);
+
+        ChangeStateTo(SlotState.Full);
+    }
+
+    public void CreateItem3D(int id)
+    {
+        var itemGO = (GameObject)Instantiate(Resources.Load("Prefabs/Grid/Item3D"));
+        
+        itemGO.transform.SetParent(this.transform);
+        itemGO.transform.localPosition = Vector3.zero;
+        itemGO.transform.localScale = Vector3.one;
+
+        currentItem = itemGO.GetComponent<Item>();
+        currentItem.Init3D(id, this);
+
+        ChangeStateTo(SlotState.Full);
+    }
+
+    private void ChangeStateTo(SlotState targetState)
+    {
+        state = targetState;
+    }
+
+    public void ItemGrabbed()
+    {
+        Destroy(currentItem.gameObject);
+        ChangeStateTo(SlotState.Empty);
+    }
+
+    private void ReceiveItem(int id)
+    {
+        switch (state)
+        {
+            case SlotState.Empty: 
+
+                CreateItem(id);
+                ChangeStateTo(SlotState.Full);
+                break;
+
+            case SlotState.Full: 
+                if (currentItem.id == id)
+                {
+                    //Merged
+                    Debug.Log("Merged");
+                }
+                else
+                {
+                    //Push item back
+                    Debug.Log("Push back");
+                }
+                break;
+        }
+    }
+}
+
+public enum SlotState
+{
+    Empty,
+    Full
+}
